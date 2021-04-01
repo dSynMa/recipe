4beb13ba71e96776141227e2c5086037b72cda08
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2021-04-01 12:14:50 +0200

    allowing state agent to be set later

--- src/main/java/recipe/lang/agents/State.java
+++ src/main/java/recipe/lang/agents/State.java
@@ -1,13 +1,28 @@
 package recipe.lang.agents;
 
 public class State<T> {
+
+    public String agent;
     public T label;
 
     public State(T label){
         this.label = label;
     }
 
+    public State(String agent, T label){
+        this.agent = agent;
+        this.label = label;
+    }
+
     public T getLabel(){
         return label;
+    }
+
+    public String getAgent(){
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
     }
 }
