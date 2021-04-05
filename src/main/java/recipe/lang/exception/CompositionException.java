9ec997195936a9547de1b1d4ede4c863880ac035
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2021-04-05 10:12:36 +0200

    generalised composition exception

---
+++ src/main/java/recipe/lang/exception/CompositionException.java
@@ -0,0 +1,15 @@
+package recipe.lang.exception;
+
+public class CompositionException extends Exception {
+
+	private static final long serialVersionUID = 3L;
+
+	public CompositionException(){
+
+	}
+
+	public CompositionException(String message){
+		super(message);
+	}
+
+}
--- src/main/java/recipe/lang/exception/CompositionWithSameAgentNameException.java
+++
@@ -1,7 +0,0 @@
-package recipe.lang.exception;
-
-public class CompositionWithSameAgentNameException extends Exception {
-
-	private static final long serialVersionUID = 3L;
-
-}
