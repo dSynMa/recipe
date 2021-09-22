e5e7dfd4e6f969147890cd817d8a3b3009b1fa5d
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2021-09-22 17:45:43 +0200

    logic to have an underlying system that can be updated, instead of reparsing for every action

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,30 +4,46 @@ import flak.Request;
 import flak.annotations.Route;
 import org.json.JSONObject;
 import org.petitparser.context.ParseError;
-import recipe.analysis.NuXmvSimulation;
+import recipe.analysis.NuXmvInteraction;
 import recipe.analysis.ToNuXmv;
 import recipe.lang.System;
 import recipe.lang.types.Enum;
 import recipe.lang.utils.Pair;
 
-import java.awt.*;
-import java.net.URI;
-
 public class Server {
-    NuXmvSimulation nuXmvSimulation;
+    NuXmvInteraction nuXmvInteraction;
+    System system;
 
     @Route("/")
     public String index() {
         return "Started";
     }
 
-    @Route("/toDOT")
-    public String toDOT(Request req) {
+    @Route("/setSystem")
+    public String setSystem(Request req) {
         Enum.clear();
         String script = req.getQuery().get("script").trim();
         try {
-            recipe.lang.System system = recipe.lang.System.parser().end().parse(script).get();
-            String toReturn = "{ \"agents\": [" + String.join(",", system.toDOT()) + "]}";
+            system = recipe.lang.System.parser().end().parse(script).get();
+            return "{}";
+        } catch (ParseError parseError){
+            return "{ \"error\" : \"" + parseError.getFailure().toString() + "\"}";
+        } catch (Exception e) {
+            return "{ \"error\" : \"" + e.getMessage() + "\"}";
+        }
+    }
+
+    @Route("/toDOT")
+    public String toDOT(Request req) {
+        if(system == null){
+            return "{ \"error\" : \"Set system by sending your script to \\setSystem.\"}";
+        }
+        try {
+            String toReturn = "{ \"agents\": [" + String.join(",", system.toDOT()) + "]";
+            if(system.isSymbolic()){
+                toReturn += ", \"symbolic\" : true";
+            }
+            toReturn += "}";
             return toReturn;
         } catch (ParseError parseError){
             return "{ \"error\" : \"" + parseError.getFailure().toString() + "\"}";
@@ -38,16 +54,31 @@ public class Server {
 
     @Route("/modelCheck")
     public String modelCheck(Request req) throws Exception {
-        String script = req.getQuery().get("script");
+        if(system == null){
+            return "{ \"error\" : \"Set system by sending your script to \\setSystem.\"}";
+        }
+
         try {
-            recipe.lang.System system = recipe.lang.System.parser().parse(script).get();
             if(system.getLtlspec() == null || system.getLtlspec().size() == 0){
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("error", "No specifications to model check.");
                 return jsonObject.toString();
             } else{
-                String out = ToNuXmv.nuxmvModelChecking(system);
                 JSONObject jsonObject = new JSONObject();
+                String out = ToNuXmv.nuxmvModelChecking(system);
+                if(out.equals("")){
+                    nuXmvInteraction = new NuXmvInteraction(ToNuXmv.transform(system));
+                    for(int i = 0; i < system.getLtlspec().size(); i++) {
+                        String spec = system.getLtlspec().get(i).replaceAll("^ *[^ ]+ +", "");
+                        Pair<Boolean, String> result = nuXmvInteraction.symbolicModelCheck(spec, 20);
+                        if(result.getLeft()) {
+                            out += spec + ":\n" + result.getRight() + "\n";
+                        } else{
+                            out += spec + " (error) :\n" + result.getRight() + "\n";
+                        }
+                    }
+                    nuXmvInteraction.stopNuXmvThread();
+                }
                 jsonObject.put("result", out);
                 return jsonObject.toString();
             }
@@ -62,13 +93,16 @@ public class Server {
 
     @Route("/simulateInit")
     public String simulateInit(Request req) throws Exception {
-        String script = req.getQuery().get("script");
+        if(system == null){
+            return "{ \"error\" : \"Set system by sending your script to \\setSystem.\"}";
+        }
 
         try {
-            nuXmvSimulation = new NuXmvSimulation(ToNuXmv.transform(System.parser().end().parse(script).get()));
-            Pair<Boolean, String> out = nuXmvSimulation.initialise();
+            nuXmvInteraction = new NuXmvInteraction(ToNuXmv.transform(system));
+            nuXmvInteraction.symbolic = system.isSymbolic();
+            Pair<Boolean, String> out = nuXmvInteraction.initialise();
             if(out.getLeft()){
-                return NuXmvSimulation.outputToJSON(out.getRight()).toString();
+                return NuXmvInteraction.outputToJSON(out.getRight()).toString();
             } else{
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("error", out.getRight());
@@ -85,9 +119,9 @@ public class Server {
 
 //    @Route("/simulatePickState")
 //    public String simulatePickState() throws Exception {
-//        Pair<Boolean, String> out = nuXmvSimulation.simulation_pick_init_state();
+//        Pair<Boolean, String> out = nuXmvInteraction.simulation_pick_init_state();
 //        if(out.getLeft()){
-//            return NuXmvSimulation.outputToJSON(out.getRight()).toString();
+//            return NuXmvInteraction.outputToJSON(out.getRight()).toString();
 //        } else{
 //            return new JSONObject("{ error: " + out.getLeft() + "}").toString();
 //        }
@@ -95,16 +129,31 @@ public class Server {
 
     @Route("/simulateNext")
     public String simulateNext(Request req) throws Exception {
+        if(system == null){
+            return "{ \"error\" : \"Set system by sending your script to \\setSystem.\"}";
+        }
+        if(nuXmvInteraction == null) {
+            String outp = simulateInit(req);
+            if(outp.contains("error")){
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("error", outp);
+                return jsonObject.toString();            }
+        }
         //if keep-all then return stuck
         String sendTransitionLabel = req.getQuery().get("constraint");
-        Pair<Boolean, String> out = nuXmvSimulation.simulation_next(sendTransitionLabel);
+        Pair<Boolean, String> out = nuXmvInteraction.simulation_next(sendTransitionLabel);
         if(out.getLeft()){
-            return NuXmvSimulation.outputToJSON(out.getRight()).toString();
+            return NuXmvInteraction.outputToJSON(out.getRight()).toString();
         } else{
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("error", out.getRight());
             return jsonObject.toString();
         }
+    }
+
+    @Route("/resetSimulation")
+    public void resetSimulation(Request req) throws Exception {
+        nuXmvInteraction = null;
     }
 
     public static void main(String[] args) throws Exception {
