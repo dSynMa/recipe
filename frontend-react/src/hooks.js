a6557da5eec087754f01470935e164feeec24404
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2021-11-23 17:25:45 +0100

    reactjs frontend

---
+++ frontend-react/src/hooks.js
@@ -0,0 +1,17 @@
+// hooks.js
+import { useState, useEffect } from "react";
+function useFetch(url) {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  async function fetchUrl() {
+    const response = await fetch(url);
+    const text = await response.text();
+    setData(text);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [data, loading];
+}
+export { useFetch };
