2b80ad5c935e087f35c6d1edeb48126098e300c4
shaunazzopardi <shaun.azzopardi@gmail.com>
2022-06-27 08:55:40 +0200

    basic bigor and bigand classes for LTOL

---
+++ src/main/java/recipe/lang/ltol/BigOr.java
@@ -0,0 +1,34 @@
+package recipe.lang.ltol;
+
+import recipe.lang.expressions.TypedVariable;
+import recipe.lang.expressions.predicate.Condition;
+import recipe.lang.utils.Triple;
+
+import java.util.Map;
+
+public class BigOr extends LTOL{
+    TypedVariable var;
+    Condition condition;
+    LTOL ltol;
+
+    public BigOr(TypedVariable var, Condition condition, LTOL ltol){
+        this.condition = condition;
+        this.var = var;
+        this.ltol = ltol;
+    }
+
+    public String toString(){
+        return "/\\" + var.toString() + "." + condition.toString() + "(" + ltol.toString() + ")";
+    }
+
+    //TODO
+    public boolean isPureLTL() {
+        return ltol.isPureLTL();
+    }
+
+    //TODO
+    @Override
+    public Triple<Integer, Map<String, Observation>, LTOL> abstractOutObservations(Integer counter) {
+        return null;
+    }
+}
