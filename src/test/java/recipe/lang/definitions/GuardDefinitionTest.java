31298c4758e740193af34c36e5d1a739435b6a71
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2023-02-24 10:00:12 +0100

    added guard definition test that checks there are no problems when variable names have a common non-zero prefix

--- src/test/java/recipe/lang/definitions/GuardDefinitionTest.java
+++ src/test/java/recipe/lang/definitions/GuardDefinitionTest.java
@@ -23,6 +23,17 @@ public class GuardDefinitionTest {
     }
 
     @Test
+    public void parser2() {
+        Guard.clear();
+        Parser parser = GuardDefinition.parser(new TypingContext());
+
+        String script = "guard g0(p : bool, p2: bool) := (p && p2);";
+        org.petitparser.context.Result r = parser.parse(script);
+        if(r.isFailure()) System.out.println(r.getMessage() + "\n" + script.substring(r.getPosition()));
+        assert r.isSuccess();
+    }
+
+    @Test
     public void testParser() {
     }
 }
