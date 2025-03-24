da70858071b6388cb6321bdee2b63e3a0ea5d56a
Luca Di Stefano <lucadistefano90@gmail.com>
2025-03-24 15:32:01 +0100

    Fix PredicateLocation class

--- src/main/java/recipe/lang/expressions/location/PredicateLocation.java
+++ src/main/java/recipe/lang/expressions/location/PredicateLocation.java
@@ -2,17 +2,18 @@ package recipe.lang.expressions.location;
 
 import org.petitparser.parser.Parser;
 
+import recipe.lang.expressions.Expression;
 import recipe.lang.expressions.TypedValue;
 import recipe.lang.expressions.predicate.Condition;
 import recipe.lang.utils.TypingContext;
 
 public class PredicateLocation extends Location {
-    Condition predicate;
+    Expression<recipe.lang.types.Boolean> predicate;
 
-    public PredicateLocation(Condition c) { this.predicate = c; }
+    public PredicateLocation(Expression<recipe.lang.types.Boolean> c) { this.predicate = c; }
     
     @Override
-    public Condition getPredicate(TypedValue supplier) { return predicate; }
+    public Expression<recipe.lang.types.Boolean> getPredicate(TypedValue supplier) { return predicate; }
 
     public static org.petitparser.parser.Parser parser(TypingContext context) throws Exception {
         Parser messageGuard = Condition.typeParser(context);
